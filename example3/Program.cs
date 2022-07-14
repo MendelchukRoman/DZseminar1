95bb0bb065a0314664e604b385f482c6d95dd275
kadzuki <heaven.roma@mail.ru>
2022-07-14 23:36:59 +0300

    Седалали задачу 3

---
+++ example3/Program.cs
@@ -0,0 +1,25 @@
+﻿// задача 3: принимает на вход число, и определяет, является ли оно четным (делится без остатка)
+ int a = 3;
+ int b = 4;
+ int c = 7;
+  if (a % 2 == 0)
+  {
+    Console.WriteLine ("Да");
+  }
+  else{
+    Console.WriteLine ("Нет");
+  }
+  if (b % 2 == 0)
+  {
+    Console.WriteLine ("Да");
+  }
+  else{
+    Console.WriteLine ("Нет");
+  }
+  if (c % 2 == 0)
+  {
+    Console.WriteLine ("Да");
+  }
+  else{
+    Console.WriteLine ("Нет");
+  }
