f6cced3de09ee3de9f79449e2184a1675ee78a2e
kadzuki <heaven.roma@mail.ru>
2022-07-14 23:16:11 +0300

    Седалали задачу 2

---
+++ example2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача: на входе 3 числа, на выход большее
+
+int a = 1;
+int b = 3;
+int c = 5;
+if (a > b && a > c)
+                    Console.WriteLine("max = " + a); 
+                else if (b > c)
+                    Console.WriteLine("max = " + b); 
+                else
+                    Console.WriteLine("max = " + c); 
+            
+            Console.ReadLine();
